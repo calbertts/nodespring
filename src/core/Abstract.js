975f25c29db4eb9c2d680588442c395a5cab9c1a
Carlos Alberto <calbertts@gmail.com>
2016-10-24 10:21:34 +0200

    Removing new.target cause support with older NodeJS versions

--- src/core/Abstract.js
+++ src/core/Abstract.js
@@ -9,7 +9,8 @@ import NodeSpringException from '../exceptions/NodeSpringException'
 
 export default class Abstract {
   constructor() {
-    if (new.target === Abstract || Object.getPrototypeOf(this.constructor).name === 'Abstract') {
+  	// new.target ===  ... removed because need to downgrade to 4.6.0
+    if (Abstract || Object.getPrototypeOf(this.constructor).name === 'Abstract') {
       let noInstantiable = new NodeSpringException("Cannot construct "+this.constructor.name+" instances directly", this, 1);
       NodeSpringUtil.throwNodeSpringException(noInstantiable)
     }
